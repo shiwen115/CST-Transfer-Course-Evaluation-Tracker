3b639e4ebd43f1be4a7664a30527733b5ac67eb6
nshupinski <55292866+nshupinski@users.noreply.github.com>
2020-11-16 19:55:17 -0500

    Added thing

--- CST Transfer UI Project/CST Transfer UI Project/Master_Page.Master.designer.cs
+++ CST Transfer UI Project/CST Transfer UI Project/Master_Page.Master.designer.cs
@@ -40,6 +40,15 @@ namespace CST_Transfer_UI_Project {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// Page_Name control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder Page_Name;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
